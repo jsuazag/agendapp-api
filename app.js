a9f2ac6c9f526218401b0b735d168a284aac7794
Jerson Suaza <jerson.suaza@gmail.com>
2021-05-06 21:16:19 -0500

    fix package.json environment

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 4000;
-//juan.ramirez@nmksoftware.co;jhonathan.cordoba@nmksoftware.co
 
 import api from './api';
 
